d544d1ce80f8e052693e6b8d5935267c7854b133
sahadul <sahadul80@gmail.com>
2025-08-02 19:54:28 +0600

    buit error fix 3

--- src/app/components/Demand/TopBar.tsx
+++ src/app/components/Demand/TopBar.tsx
@@ -1,16 +1,21 @@
 'use client'
-import { useEffect } from "react";
 import { LogOutButton } from "../LogOutButton";
+import { useState, useEffect } from 'react'; // Import hooks
 
 export default function TopBar() {
-  
-  const user = localStorage.getItem('user')!;
+  const [user, setUser] = useState<string | null>(null); // Initialize state
+
+  useEffect(() => {
+    // This effect runs only on the client side
+    setUser(localStorage.getItem('user'));
+  }, []);
+
   return (
     <div className="sticky top-0 z-10 bg-gray-50 dark:bg-gray-900 py-3 px-2 sm:px-4 flex justify-between items-center mb-2 sm:mb-4 border-b">
       <h1 className="text-lg sm:text-xl font-bold text-gray-800 dark:text-white">
         Demand Form
       </h1>
-      {user ? (<LogOutButton username={user }/>) : (<></>)}
+      {user ? <LogOutButton username={user} /> : null}
     </div>
   );
 }
